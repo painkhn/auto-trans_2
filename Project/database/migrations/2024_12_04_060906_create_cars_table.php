db127bd83cecdd90f6300b4d280380cfc5a0c3bf
1001001010 <lll100100@yandex.ru>
2024-12-04 17:20:35 +0800

    модель car

---
+++ Project/database/migrations/2024_12_04_060906_create_cars_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('price');
+            $table->integer('mileage');
+            $table->year('year');
+            $table->enum('engine', ['gasoline', 'diesel', 'electro']);
+            $table->decimal('volume', 2, 1);
+            $table->integer('power');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
