9cf5c155c24ea6d8c8e8677155da47351515ee94
1001001010 <lll100100@yandex.ru>
2024-12-12 13:23:14 +0800

    карта

--- Project/resources/views/index.blade.php
+++ Project/resources/views/index.blade.php
@@ -143,6 +143,15 @@
                     </div>
                 </li>
             </ul>
+            <div>
+                <h2 class="text-center font-black text-5xl max-w-3xl mx-auto my-0 text-gray-800 mt-40 mb-10">
+                    Как нас найти
+                </h2>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d444.72060371505194!2d104.28680612490751!3d52.223366487453426!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5da83ce19e66fddf%3A0x2d44f9268ebeb3c8!2z0J_RgdC40YXQuNCw0YLRgNC40YfQtdGB0LrQsNGPINCR0L7Qu9GM0L3QuNGG0LA!5e0!3m2!1sru!2sru!4v1733980703366!5m2!1sru!2sru"
+                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade" class="w-full"></iframe>
+            </div>
             <h2 class="text-center font-black text-5xl max-w-3xl mx-auto my-0 text-gray-800 mt-40 mb-10">
                 Контактная информация
             </h2>
